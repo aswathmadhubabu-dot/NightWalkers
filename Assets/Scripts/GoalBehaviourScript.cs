c793400505630358f09b55644b85f57b969d9efc
Lucas Hamie <lucas.hamie@gmail.com>
2021-06-05 14:51:00 -0300

    Initial Commit

---
+++ Assets/Scripts/GoalBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBehaviourScript : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject ballSpawn;
+    public TeamScript team;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ball = new GameObject();
+
+    }
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == ball) { 
+            //Goal!
+            ball.transform.position = ballSpawn.transform.position;
+            ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            team.score = team.score + 1;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    //public TextMeshProUGUI countText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+         
+        //SetCountText();
+
+    }
+
+    /*void SetCountText()
+    {
+        countText.text = "Count " + count.ToString();
+        if(count >= 6)
+        {
+        }
+    }*/
+    // Update is called once per frame
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    // Update is called once per frame
+    void OnJump(InputValue movementValue)
+    {
+        rb.AddForce(new Vector3(0.0f, 4.8f, 0.0f), ForceMode.Impulse);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movementVector = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movementVector * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            //count++;
+            //SetCountText(); 
+        }
+    }
+}
