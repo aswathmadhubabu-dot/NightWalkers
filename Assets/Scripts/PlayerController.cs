b61e782f90b422aaee032d3d48e2315a0595aae2
Lucas Hamie <lucas.hamie@gmail.com>
2021-06-19 00:47:35 -0300

    Enhanced moivement, jumping and dashing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,19 +13,29 @@ public class PlayerController : MonoBehaviour
     private float movementY;
     //public TextMeshProUGUI countText;
     private Animator anim;
+    private Collider collider;
     public float forwardMaxSpeed;
     public Camera cam;
 
     public Transform spine;
-
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    private bool jumping = false;
+    private bool isGrounded = true;
+    private bool attacking = false;
+    private Vector3 directionFacing;
+    private Vector3 pointerDirection;
+    private Vector2 mousepos;
+    private bool isDashing = false;
+    public float attackDashStrength;
+    public float dashForceDelay;
+    private bool dashImpulsing = false;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
-
-        //SetCountText();
-
+        collider = GetComponent<Collider>();
     }
 
     /*void SetCountText()
@@ -42,53 +52,91 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
 
-        Debug.Log("onMove " + movementY.ToString());
-
-        //rb.MovePosition(rb.position + this.transform.forward * movementY */* Time.deltaTime */ forwardMaxSpeed);
+        //Debug.Log("onMove " + movementY.ToString());
 
     }
     void OnYLook(InputValue look)
     {
         
-        Vector2 mousepos = look.Get<Vector2>();
-        Debug.Log("mousepos " + mousepos.ToString() );
-        Vector3 point = cam.ScreenToWorldPoint(new Vector3(mousepos.x, mousepos.y, 1));
-        float t = cam.transform.position.y / (cam.transform.position.y - point.y);
-        Vector3 finalPoint = new Vector3(t * (point.x - cam.transform.position.x) + cam.transform.position.x, 0.4f, t * (point.z - cam.transform.position.z) + cam.transform.position.z);
-
-        transform.LookAt(finalPoint, Vector3.up);
+        mousepos = look.Get<Vector2>();
+    
     }   
     void OnLeftAttack(InputValue movementValue)
     {
         anim.SetTrigger("lattack");
+        StartCoroutine(Dash());
     }
     
 
     void OnRightAttack(InputValue movementValue)
     {
         anim.SetTrigger("rattack");
+        StartCoroutine(Dash());
     }
     // Update is called once per frame
-    void OnJump(InputValue movementValue)
+    void OnJump(InputValue input)
     {
         //anim.SetTrigger("Jump");
-        rb.AddForce(new Vector3(0.0f, 0.4f, 0.0f) * 20 * speed, ForceMode.VelocityChange);
+        jumping = Convert.ToBoolean(input.Get<float>());
+        if(jumping && isGrounded){
+            rb.AddForce(Vector3.up * 10 * speed, ForceMode.VelocityChange);
+        }
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        anim.SetFloat("vely", rb.velocity.z  );
-        anim.SetFloat("velx", rb.velocity.x  );
-
         Vector3 movementVector = new Vector3(movementX, 0.0f, movementY);
-        if(movementVector != Vector3.zero )  {
-            Debug.Log("Adding force: " + movementVector.ToString() + " " + speed);
-            rb.AddForce(movementVector * speed, ForceMode.VelocityChange);
+
+        //Debug.Log("MovementVect: " + rb.velocity.magnitude.ToString() );
+        if(movementVector == Vector3.zero){
+            if(Math.Abs(rb.velocity.x) > 0.1f || Math.Abs(rb.velocity.z) > 0.1f && isGrounded){
+                //Debug.Log("STOPPING: " + rb.velocity.magnitude.ToString() );
+                rb.AddForce(rb.velocity * -0.4f , ForceMode.Impulse);
+            }
+        } else {
+            if(isGrounded && rb.velocity.magnitude <= forwardMaxSpeed)  {
+            //Debug.Log("Adding force: " + movementVector.ToString() + " " + speed);
+                rb.AddForce(movementVector * speed, ForceMode.VelocityChange);
+            }
         }
+        if(!isDashing){
+            if(rb.velocity.y < 0){
+                rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier  - 1) * Time.deltaTime;
+            } else if(rb.velocity.y < 7f){
+                rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier  - 1) * Time.deltaTime;
+            } else if (rb.velocity.y > 0 && !jumping) {
+                rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier  - 1) * Time.deltaTime;
+            }
+        }
+
+        if(dashImpulsing){
+            dashImpulsing = false;
+            rb.AddForce(transform.rotation * Vector3.forward * attackDashStrength, ForceMode.Impulse);
+        }
+    }
+    IEnumerator Dash(){
+        isDashing = true;
+        rb.useGravity = false;
+        yield return new WaitForSeconds(dashForceDelay);
+        dashImpulsing = true;
+        yield return new WaitForSeconds(0.2f);
+        rb.useGravity = true;
+        isDashing = false;
     }
     void Update() 
     {
+        anim.SetFloat("vely", rb.velocity.z);
+        isGrounded = Physics.Raycast(collider.bounds.center, Vector3.down, collider.bounds.extents.y + 0.1f);
+
+
+        pointerDirection = cam.ScreenToWorldPoint(new Vector3(mousepos.x, mousepos.y, 1));
+        float t = cam.transform.position.y / (cam.transform.position.y - pointerDirection.y);
+        directionFacing = new Vector3(t * (pointerDirection.x - cam.transform.position.x) + cam.transform.position.x, rb.transform.position.y  , t * (pointerDirection.z - cam.transform.position.z) + cam.transform.position.z);
+
+        //anim.SetFloat("velx", rb.velocity.x  );
         // anim.SetFloat("velx", inputTurn);
+        transform.LookAt(directionFacing, Vector3.up);
+
     }
 
     private void OnTriggerEnter(Collider other)
