836dcc949d53de8eaa5136dacd6a02c548a18b85
chiragshenoy <chiragshenoy@gmail.com>
2021-07-05 19:53:04 +0530

    Added timer

--- Assets/MatchController.cs
+++ Assets/MatchController.cs
@@ -5,63 +5,72 @@ using TMPro;
 
 public class MatchController : MonoBehaviour
 {
-
     public TextMeshProUGUI goalText;
-    public TextMeshProUGUI timeText;
+
     public int gameTimeInSecs;
-    private float remainingTime;
+
     public GameObject ball;
     public GameObject ballSpawn;
+   
     public TeamScript teamA;
-    
     public TeamScript teamB;
+    
     public bool recentGoal;
     public int goalTimeout;
 
+    [SerializeField] private Timer timer;
+
     void Start()
     {
-        remainingTime = gameTimeInSecs;
         goalText.enabled = false;
         recentGoal = false;
 
+        timer
+            .SetDuration(gameTimeInSecs)
+            .OnEnd(() => Debug.Log("Timer 1 ended"))
+            .Begin();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (remainingTime > 0)
-        {
-            remainingTime -= Time.deltaTime;
-        }
-        float minutes = Mathf.FloorToInt(remainingTime / 60);
-        float seconds = Mathf.FloorToInt(remainingTime % 60);
-        timeText.text = minutes.ToString() + " : " + seconds.ToString();
     }
 
-    public void NewGoal(TeamScript happyTeam ){
+    void DisplayTimeEnded()
+    {
+        
+    }
+
+    public void NewGoal(TeamScript happyTeam)
+    {
         goalText.text = "" + happyTeam.teamName + " Goal!";
         StartCoroutine(ResetPlayersAndBall(happyTeam));
     }
 
-    IEnumerator ResetPlayersAndBall(TeamScript happyTeam){
+    IEnumerator ResetPlayersAndBall(TeamScript happyTeam)
+    {
         recentGoal = true;
         goalText.enabled = true;
         TeamScript angryTeam;
-        if(teamA.name == happyTeam.name){
+        if (teamA.name == happyTeam.name)
+        {
             angryTeam = teamB;
-        } else {
+        }
+        else
+        {
             angryTeam = teamA;
         }
+
         setAngry(angryTeam);
         setHappy(happyTeam);
 
 
         yield return new WaitForSeconds(goalTimeout);
-        
+
         ball.transform.position = ballSpawn.transform.position;
         ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
         ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-        
+
         resetPlayerPos(teamA);
         resetPlayerPos(teamB);
         setNormal(teamA);
@@ -70,26 +79,37 @@ public class MatchController : MonoBehaviour
         goalText.enabled = false;
         recentGoal = false;
     }
-    
-    void resetPlayerPos(TeamScript team){
-        foreach (GameObject player in team.players){
+
+    void resetPlayerPos(TeamScript team)
+    {
+        foreach (GameObject player in team.players)
+        {
             player.GetComponent<Rigidbody>().velocity = Vector3.zero;
             player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             player.transform.position = team.playerSpawn.transform.position;
         }
     }
-    void setHappy(TeamScript team){
-        foreach (GameObject player in team.players){
+
+    void setHappy(TeamScript team)
+    {
+        foreach (GameObject player in team.players)
+        {
             player.GetComponent<Animator>().SetInteger("Emotion", 1);
         }
     }
-    void setAngry(TeamScript team){
-        foreach (GameObject player in team.players){
+
+    void setAngry(TeamScript team)
+    {
+        foreach (GameObject player in team.players)
+        {
             player.GetComponent<Animator>().SetInteger("Emotion", 2);
         }
     }
-    void setNormal(TeamScript team){
-        foreach (GameObject player in team.players){
+
+    void setNormal(TeamScript team)
+    {
+        foreach (GameObject player in team.players)
+        {
             player.GetComponent<Animator>().SetInteger("Emotion", 0);
         }
     }
