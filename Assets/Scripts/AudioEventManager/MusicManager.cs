0aa92bdc19dbcacab7628edfef00823d183d70ea
Marina Iaroslavtseva <miarosla3@gatech.edu>
2021-07-06 01:41:05 +0300

    Added Audio Event Manager, basic sound effects and background music

---
+++ Assets/Scripts/AudioEventManager/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance = null;
+    private AudioSource audio;
+    //public AudioClip backgroundMusic;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        if (instance == this) return;
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        //audio.clip = this.backgroundMusic;
+        audio.loop = true;
+        audio.Play();
+    }
+}
