2915657161aa4c2985f788e6b165b82c9da326ca
Salil Sharma <41990985+sllshrm@users.noreply.github.com>
2021-07-06 23:39:18 +0530

    Refined AI in team setting

---
+++ Assets/Scripts/BallReporter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReporter : MonoBehaviour
+{
+    public double distanceFromGround { get; set; }
+    public bool isGrounded { get; set; }
+
+    public float maxDistance = 50f;
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, maxDistance))
+        {
+            if (hit.transform.CompareTag("Ground"))
+            {
+                distanceFromGround = hit.distance - (this.transform.localScale.y / 2);
+
+                if (distanceFromGround < 0.5)
+                {
+                    isGrounded = true;
+                }
+                else
+                {
+                    isGrounded = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayersAIScript.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent)), RequireComponent(typeof(VelocityReporter))]
+public class PlayersAIScript : MonoBehaviour
+{
+   
+    public Transform ball, goalLocation;
+    public int ballkickForce = 2;
+    public float minimumAlignmentDistance = 4;
+    public PlayerState playerState;
+
+    private Vector3 startPosition;
+    private VelocityReporter velocityReporter;
+    private NavMeshAgent navAgent;
+
+    private Rigidbody ballRigidBody;
+    private Collider ballDetectorCollider;
+    private BallReporter ballReporter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = this.transform.position;
+        playerState = PlayerState.Idle;
+        navAgent = GetComponent<NavMeshAgent>();
+        ballRigidBody = ball.GetComponent<Rigidbody>();
+        ballReporter = ball.GetComponent<BallReporter>();
+        velocityReporter = GetComponent<VelocityReporter>();
+        ballDetectorCollider = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerState == PlayerState.Idle)
+        {
+            float distance = (this.transform.position - startPosition).magnitude;
+            if (distance > 1)
+            {
+                navAgent.SetDestination(startPosition);
+            }
+
+            if (navAgent.remainingDistance < 0.1)
+            {
+                Vector3 lookPos = ball.position - transform.position;
+                lookPos.y = 0;
+                Quaternion rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * navAgent.angularSpeed);
+            }
+        } else if (playerState == PlayerState.Attacking)
+        {
+            Vector3 goalDir = (goalLocation.transform.position - ball.position);
+            Vector3 desiredDirection = (ball.position - goalDir.normalized);
+            if ((ball.position - transform.position).magnitude > minimumAlignmentDistance) {
+                desiredDirection = desiredDirection * 2;
+            }
+            Debug.DrawLine(transform.position, desiredDirection, Color.red);
+
+            navAgent.SetDestination(desiredDirection);
+
+            if (navAgent.remainingDistance < 1)
+            {
+                if (Vector3.Angle(goalDir, this.transform.forward) < 10)
+                {
+                    this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(goalDir), 
+                        Time.deltaTime * navAgent.angularSpeed);
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            kickBall();
+        }
+    }
+
+    private void kickBall()
+    {
+        Vector3 goalDir = (goalLocation.transform.position - ball.position);
+        ballRigidBody.AddForce(this.transform.forward * (velocityReporter.velocity.magnitude * ballkickForce), ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //    Vector3 goalDir = (goalLocation.transform.position - ball.position);
+    //    Vector3 desiredDirection = (ball.position - goalDir.normalized * 2);
+    //    navAgent.SetDestination(desiredDirection);
+
+    //    if (navAgent.remainingDistance < 1) { 
+
+    //        if (Vector3.Angle(goalDir, this.transform.forward) < 10)
+    //        {
+    //            if (ballReporter.distanceFromGround > 0.01)
+    //            {
+    //                if (isGrounded)
+    //                {
+    //                    Debug.Log("In Jump");
+    //                }
+    //            }
+    //            else
+    //            {
+    //                //ballRigidBody.AddForce(this.transform.forward * velocityReporter.velocity.magnitude * 10);
+    //            }
+    //        }
+    //        else
+    //        {
+    //            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(goalDir), 
+    //                Time.deltaTime * navAgent.angularSpeed);
+    //        }
+    //    }
+    //}
+}
