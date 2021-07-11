8d4eb98be0f47d27ad042dfd67d70a8b71de383c
Aswath <aswathm@sahajsoft.com>
2021-07-11 14:27:45 +0530

    Adding GTA style car enter

---
+++ Galactik Football/Assets/Scripts/EnterCar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class EnterCar : MonoBehaviour
+{
+    public GameObject carCamera;
+    public GameObject player;
+    public GameObject exitTrigger;
+    public GameObject car;
+    public int triggerCheck;
+
+    private void OnTriggerEnter(Collider other) => triggerCheck = 1;
+
+    private void OnTriggerExit(Collider other) => triggerCheck = 0;
+
+    void Update()
+    {
+        if (triggerCheck == 1 && Input.GetButtonDown("Action"))
+        {
+            carCamera.SetActive(true);
+            player.SetActive(false);
+            ((Behaviour) car.GetComponent("CarController")).enabled = true;
+            ((Behaviour) car.GetComponent("CarUserControl")).enabled = true;
+            exitTrigger.SetActive(true);
+        }
+    }
+}
---
+++ Galactik Football/Assets/Scripts/ExitCar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitCar : MonoBehaviour
+{
+    public GameObject _carCam;
+    public GameObject _player;
+    public GameObject _exitTrigger;
+    public GameObject _car;
+    public GameObject _exitPlace;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Action"))
+        {
+            _player.SetActive(true);
+            _player.transform.position = _exitPlace.transform.position;
+            _carCam.SetActive(false);
+            ((Behaviour) _car.GetComponent("CarController")).enabled = false;
+            ((Behaviour) _car.GetComponent("CarUserControl")).enabled = false;
+            _exitTrigger.SetActive(false);
+        }
+    }
+}
