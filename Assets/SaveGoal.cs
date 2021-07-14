47f50634114d2c50d27c1a7ce70711a695f0323c
Aswath <aswathm@sahajsoft.com>
2021-07-14 22:49:39 +0530

    Adding basic goalie with animation

---
+++ Assets/SaveGoal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SaveGoal : MonoBehaviour
+{
+    public GameObject goalKeeper;
+    [SerializeField] private Animator animator;
+
+    void Start()
+    {
+        animator = goalKeeper.GetComponent<Animator>();
+        Debug.Log(animator);
+    }
+
+    private void OnTriggerEnter(Collider other) => animator.SetBool("isBallOnTop", true);
+
+    private void OnTriggerExit(Collider other) => animator.SetBool("isBallOnTop", false);
+}
