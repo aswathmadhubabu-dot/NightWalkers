7bddb54a6f369fa6f9c9e107980967e0543d3588
Aswath <aswathm@sahajsoft.com>
2021-07-21 19:55:43 +0530

    Adding maze level scene

---
+++ Assets/Scripts/GlassdoorOpen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GlassdoorOpen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator doorOpenAnimator;
+
+    void Start() => doorOpenAnimator = this.GetComponent<Animator>();
+
+    private void OnCollisionEnter(Collision other) => doorOpenAnimator.SetBool("character_nearby", true);
+
+    private void OnCollisionExit(Collision other) => doorOpenAnimator.SetBool("character_nearby", false);
+}
