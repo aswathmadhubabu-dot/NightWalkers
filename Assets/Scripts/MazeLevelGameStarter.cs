0a06367107f62f74dab842b31189f2c9b4e2fe43
Aswath <aswathm@sahajsoft.com>
2021-07-24 19:57:46 +0530

    Adding Text mesh pro for wrong maze direction and clouds

---
+++ Assets/Scripts/MazeLevelGameStarter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MazeLevelGameStarter : MonoBehaviour
+{
+    public void StartGame() => SceneManager.LoadScene("MazeLevel");
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("MazeLevel");
+        Time.timeScale = 1f;
+    }
+}
