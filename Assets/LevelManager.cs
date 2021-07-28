025061def10547a40ccc7bd21f2b6591b3b3fbbd
Aswath <aswathm@sahajsoft.com>
2021-07-28 08:21:19 +0530

    Adding game instructions to medium level

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void FirstLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SimpleLevel");
+    }
+
+    public void SecondLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MediumLevel");
+    }
+
+    public void ThirdLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MazeLevel");
+    }
+}
